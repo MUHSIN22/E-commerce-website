39d00ea169d836614edf29d4bd42246bffc1d906
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-03-16 21:31:11 +0530

    make a flow in folders etc.,....

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const hbs = require('express-handlebars')
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout',partialsDir:__dirname+'/views/partials/'}))
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,28 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let products = [
     {
-      name:""
+      price: "100000",
+      name: "I Phone 11",
+      image : "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg"
+    },
+    {
+      price: "100000",
+      name: "I Phone 11",
+      image : "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg"
+    },
+    {
+      price: "100000",
+      name: "I Phone 11",
+      image : "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg"
+    },
+    {
+      price: "100000",
+      name: "I Phone 11",
+      image : "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg"
     }
   ]
 
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Shopping cart',products,admin:false});
 });
 
 module.exports = router;
